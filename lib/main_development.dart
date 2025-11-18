328efacb351c4b1ad9b55530cefedb2b906c0699
iamutaz <mutaz.alhomsi.sy@gmail.com>
2025-11-18 06:46:11 +0300

    just added the dev and production flavro to the project

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,12 @@
 import 'package:docdoc/core/DI/get_it.dart';
 import 'package:docdoc/core/Routing/generateroute.dart';
-import 'package:docdoc/featuers/home.dart';
+import 'package:docdoc/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetit();
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
---
+++ lib/main_production.dart
@@ -0,0 +1,20 @@
+import 'package:docdoc/core/DI/get_it.dart';
+import 'package:docdoc/core/Routing/generateroute.dart';
+import 'package:docdoc/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  setupGetit();
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Home(generateroute: Generateroute());
+  }
+}
